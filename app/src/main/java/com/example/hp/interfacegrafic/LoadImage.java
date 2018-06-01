dd4ba25c647a99e0c26e728645e54b54412860b2
Daniel Esteban <cyberseIA@gmail.com>
2018-06-01 18:07:15 -0400

    Miau :v

--- app/src/main/java/com/example/hp/interfacegrafic/LoadImage.java
+++ app/src/main/java/com/example/hp/interfacegrafic/LoadImage.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,6 +31,7 @@ public class LoadImage extends AppCompatActivity
     ImageView imagen;
     String path="";
 
+    Button BotonCargar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -37,7 +39,10 @@ public class LoadImage extends AppCompatActivity
         setContentView(R.layout.activity_load_image);
 
         imagen = (ImageView)findViewById(R.id.imageLoad);
+
     }
+
+
 
     public void onClick(View view)
     {
