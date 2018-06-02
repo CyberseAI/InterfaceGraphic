42d8fe60514e195967c838458c08baf3bdd0ab4f
= <cyberseIA@gmail.com>
2018-06-01 23:30:18 -0400

    Prueba 3

--- app/src/main/java/com/example/hp/interfacegrafic/MenuLogeado.java
+++ app/src/main/java/com/example/hp/interfacegrafic/MenuLogeado.java
@@ -1,13 +1,35 @@
 package com.example.hp.interfacegrafic;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuLogeado extends AppCompatActivity {
+public class MenuLogeado extends AppCompatActivity
+{
+    private Context root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_logeado);
+
+        root=this;
+        loadcomponents();
+    }
+
+    private void loadcomponents()
+    {
+        Button btn = (Button)this.findViewById(R.id.btnInmueble);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent menu = new Intent(root, FormCasas.class);
+                root.startActivity(menu);
+            }
+        });
     }
 }
