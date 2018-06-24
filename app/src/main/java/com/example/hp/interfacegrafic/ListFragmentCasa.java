d70ecf46bac3cfb4a3b7b60ee8be39f86b6c8ac6
Maritza <lunita.ma.123@gmail.com>
2018-06-24 16:21:52 -0400

    inter

--- app/src/main/java/com/example/hp/interfacegrafic/ListFragmentCasa.java
+++ app/src/main/java/com/example/hp/interfacegrafic/ListFragmentCasa.java
@@ -36,7 +36,7 @@ public class ListFragmentCasa extends Fragment {
 
     private void loadData() {
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://10.10.1.150:7777/api/v1.0/inmuebles_ecp", new JsonHttpResponseHandler() {
+        client.get("http://192.168.43.150:7777/api/v1.0/inmuebles_ecp", new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
